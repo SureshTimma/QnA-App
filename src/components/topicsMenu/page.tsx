9faee55433c99fe6983e3a5ba906c5c14e96285f
SureshTimma <148480722+SureshTimma@users.noreply.github.com>
2025-06-17 09:52:01 +0530

    vercel deployment ready

--- src/components/topicsMenu/page.tsx
+++ src/components/topicsMenu/page.tsx
@@ -3,8 +3,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+interface Topic {
+  id: string;
+  topicName: string;
+}
+
 const TopicsMenu = () => {
-  const [topics, setTopics] = useState([]);
+  const [topics, setTopics] = useState<Topic[]>([]);
 
   useEffect(() => {
     const fetchTopics = async () => {
